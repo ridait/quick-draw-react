f312cdd1ef9c026c12c4330b90e844031dee38a8
ridait <rejra2012@gmail.com>
2018-12-16 13:07:54 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <React.Fragment />;
+  }
+}
+
+export default App;
